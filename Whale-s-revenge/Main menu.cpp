16522e9b127b94a9d2dc47656c24ed97d5f25bb5
cbobeda <cbobeda@gaming.tech>
2024-12-09 16:10:56 +0100

    Ajout du bouton d'option

--- Whale-s-revenge/Main menu.cpp
+++ Whale-s-revenge/Main menu.cpp
@@ -1,9 +1,11 @@
 ﻿#include "Main menu.h"
 
 button play_button(100,100,100,100);
+button option_button(100,300,100,100);
 bool active = true;
 bool frame = false;
 bool isready = false;
+bool option = false;
 Clock watch;
 float frameDuration = 0.5f; // Durée d'une frame (en secondes)
 size_t currentFrame = 0;
@@ -35,6 +37,14 @@ bool Main_menu::mdisplay(RenderWindow& window)
     text.setOrigin(262,26.5);
     text.setPosition(Vector2f(960,200));
     text.setScale(1.5,1.5);
+    Text option_text;
+    option_text.setFont(font);
+    option_text.setCharacterSize(40);
+    option_text.setFillColor(Color::White);
+    option_text.setOutlineColor(Color::Black);
+    option_text.setOutlineThickness(5);
+    option_text.setPosition(Vector2f(960,500));
+    option_text.setString("OPTION");
     std::cout << text.getLocalBounds().getSize().y << std::endl;
    
     Texture background;
@@ -52,13 +62,25 @@ bool Main_menu::mdisplay(RenderWindow& window)
     background_sprite.setTexture(background);
     //if (true) window.setPosition(sf::Vector2i(0 + rand() % 25, 0+ rand() % 25));
     window.draw(background_sprite);
-    window.draw(play_button.bdisplay(Color::Green,Color::Black));
+    if (option)
+    {
+        window.draw(option_text);
+    }
+    else
+    {
+        window.draw(play_button.bdisplay(Color::Green,Color::Black));
+        window.draw(option_button.bdisplay(Color::Red,Color::Black));
+    }
     window.draw(text);
     std::cout << Mouse::getPosition(window).x << std::endl;
     std::cout << Mouse::getPosition(window).y << std::endl;
     if (play_button.check(Mouse::getPosition().x, Mouse::getPosition().y, window) && Mouse::isButtonPressed(Mouse::Left))
     {
         active = false;
+    }
+    if (option_button.check(Mouse::getPosition().x, Mouse::getPosition().y, window) && Mouse::isButtonPressed(Mouse::Left))
+    {
+        option = true;
     }
     if (!active)
         window.clear();
